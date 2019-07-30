8b4507985939c2f1f8e5909e37bc94f0457a9472
Karlo Smid <karlo.smid@gmail.com>
2019-07-30 16:25:31 +0200

    Add positives tail recursion version.

--- solutions/my_recursion.ex
+++ solutions/my_recursion.ex
@@ -1,14 +1,36 @@
+defmodule ElixirInAction.Recursion do
+  def positive([], _) do
+    []
+  end
+  def positive([head|tail], :false) when is_number(head) == :false, do: positive(tail, :false)
+  def positive([head|tail], :false) when rem(head, 2) == 0, do: [head] ++ positive(tail, :false)
+  def positive([_|tail], :false) do
+    positive(tail, :false)
+  end
+  def positive(list, :true) do
+    do_positive([], list)
+  end
+  defp do_positive(positives,[head|tail]) when is_number(head) == :false, do: do_positive(positives, tail)
+  defp do_positive(positives, [head|tail]) when rem(head, 2) == 0, do: do_positive(positives ++ [head], tail)
+  defp do_positive(positives, [_|tail]) do
+    do_positive(positives,tail) 
+  end
+  defp do_positive(positives, []) do
+    positives 
+  end
+end
 defmodule ElixirInAction.Recursion.Range do
   def int(from, to, _) when is_integer(from) == :false or is_integer(to) == :false, do: "from and to must be integer."
   def int(from, to, _) when from == to, do: [from]
   def int(from, to, _) when to < from, do: []
   @spec int(integer(), integer(), atom()) :: list(integer())
-  def int(from, to, :true) do
+  def int(from, to, tail = :false) do
+    [from] ++ int(from+1, to, tail) 
+  end
+  def int(from, to, _ = :true) do
     do_int_tail(from+1, to, [from])
   end
-  def int(from, to, :false) do
-    [from] ++ int(from+1, to, :false) 
-  end
+  
   defp do_int_tail(from, to, range) when from == to, do: [from|range] |> Enum.reverse()
   defp do_int_tail(from, to, range) do
     do_int_tail(from+1, to, [from|range])  
