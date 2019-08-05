a78f5b81b77a719d64e594313d3552f3102a1177
Karlo Smid <karlo.smid@gmail.com>
2019-08-05 11:06:27 +0200

    Use for entry map of date and entity.

--- solutions/ch04.ex
+++ solutions/ch04.ex
@@ -1,7 +1,7 @@
 defmodule TodoList do
   def new, do: MultiDict.new()
-  def add_entry(todo_list, date, title) do
-    MultiDict.add(todo_list, date, title)
+  def add_entry(todo_list, entry) do
+    MultiDict.add(todo_list, entry.date, entry)
   end
   def entries(todo_list, date) do
     MultiDict.get(todo_list, date)
