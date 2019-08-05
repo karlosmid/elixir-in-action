9f71e6000a8c7231d4e537f67191bfd1899cb106
Karlo Smid <karlo.smid@gmail.com>
2019-08-05 10:45:50 +0200

    Add MultiDict as new abstraction for storing entities.

--- solutions/ch04.ex
+++ solutions/ch04.ex
@@ -7,3 +7,12 @@ defmodule TodoList do
     Map.get(todo_list, date, [])
   end
 end
+defmodule MultiDict do
+  def new, do: %{}
+  def add(dict, key, value) do
+    Map.update(dict, key, [value], &[value | &1])
+  end
+  def get(dict, key) do
+    Map.get(dict, key, [])
+  end
+end
