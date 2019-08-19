ceb470de4b5940da9b222bf660aec267d0bf699f
Karlo Smid <karlo.smid@gmail.com>
2019-08-19 22:21:34 +0200

    Add state to Database server. No Interface change.

--- solutions/ch05.ex
+++ solutions/ch05.ex
@@ -21,7 +21,10 @@ run_query =
 
 defmodule DatabaseServer do
   def start do
-    spawn(&loop/0)
+    spawn(fn ->
+      connection = :rand.uniform(1000)
+      loop(connection)
+      end)
   end 
   def run_async(server_pid, query_def) do
     send(server_pid, {:run_query, self(), query_def})
@@ -33,16 +36,17 @@ defmodule DatabaseServer do
       5000 -> {:error, :timeout}
     end
   end
-  defp loop do
+  defp loop(connection) do
     receive do
-      {:run_query, caller, query_def} ->
-        send(caller, {:query_result, run_query(query_def)})
-    loop()
+      {:run_query, from_pid, query_def} ->
+        query_result = run_query(connection, query_def)
+        send(from_pid, {:query_result, query_result})
+    loop(connection)
     end
   end
-  defp run_query(query_def) do
+  defp run_query(connection, query_def) do
     Process.sleep(2000)
-    "#{query_def} result."
+    "Connection #{connection}: #{query_def} result."
   end
 end
     
